f78e244583515794f86b9042d0a715bb0007db1f
Doruk Demirci <dorukdemirci.boun@gmail.com>
2022-05-08 22:17:26 +0300

    WebAPI swagger implementation and feeding in-memory database were completed.

--- EverybodyCodes.WebAPI/Program.cs
+++ EverybodyCodes.WebAPI/Program.cs
@@ -1,10 +1,25 @@
+using EverybodyCodes.Data;
+using EverybodyCodes.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CameraDetailsContext>(x => x.UseInMemoryDatabase("cameraDetails"));
+
+// Configuring Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
@@ -14,4 +29,31 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//Set in memory database
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetService<CameraDetailsContext>();
+    
+    if (context == null) return;
+    FillDatabase(context);
+}
+
 app.Run();
+
+
+void FillDatabase(CameraDetailsContext context)
+{
+    string path = $"{app.Environment.ContentRootPath}\\DataSource\\cameras-defb.csv";
+
+    string[] lines = File.ReadAllLines(path);
+    foreach (string line in lines)
+    {
+        string[] columns = line.Split(';');
+        if (columns.Length < 3) continue;
+
+        if (!double.TryParse(columns[1], out var latitude) || !double.TryParse(columns[2], out var longitude)) continue;
+
+        context.CameraDetails.Add(new CameraDetails(columns[0], latitude, longitude));
+    }
+    context.SaveChanges();
+}
